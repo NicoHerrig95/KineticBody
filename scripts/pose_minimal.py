5969d272e006006b0ddfde12964fecf294008e7c
Nico Herrig <nico.herrig@eviden.com>
2026-03-12 15:17:39 +0100

    added visualization functions for video and image

--- scripts/pose_minimal.py
+++ scripts/pose_minimal.py
@@ -1,12 +1,14 @@
 import os 
 import sys
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_DIR)
 import cv2
 import mediapipe as mp
 import numpy as np 
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, BASE_DIR)
+from utils.common import read_json
 from model.model_base_class import POSE
+from model.body import KineticBody
+from model.visualization import visualize_video
 
 IMAGE_PATH = "./data/images/treadmill_test_img.PNG"
 VIDEO_PATH = "./data/videos/treadmill_test_vid.MOV"
@@ -16,15 +18,23 @@ MODEL_PATH = "pose_landmarker_lite.task"
 
 if __name__ == "__main__":
     MODE = sys.argv[1]
+
     model = POSE(mode = MODE)
     if MODE == "image":
         body = model(IMAGE_PATH)        
         body.save("positions_image.json")
         # body.visualize(background = cv2.imread(IMAGE_PATH))
     elif MODE == "video":
-        body = model(VIDEO_PATH)
-        # saving body 
-        body.save("positions_video.json")
-        print(body.UpperArmRight)
-    
-    
+        # body = model(VIDEO_PATH)
+        # # saving body 
+        # body.save("positions_video.json")
+        # print(body.UpperArmRight)
+        positions = read_json("positions_video.json")
+        pos = positions["positions"]
+        meta = positions["meta"]
+        body = KineticBody(
+            positions=pos,
+            metadata=meta
+        )
+        
+        visualize_video(body = body,capture=cv2.VideoCapture(VIDEO_PATH))

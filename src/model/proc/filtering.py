0c218974f67525e9f58c4746e25d49210b802ccd
NicHrg <nico.herrig@gmx.de>
2026-03-22 20:02:26 +0100

    refactoring & implemented SavGol filter

---
+++ src/model/proc/filtering.py
@@ -0,0 +1,49 @@
+import os 
+import sys 
+import numpy as np
+from scipy.signal import savgol_filter
+
+
+
+
+
+SAVGOL_DEFA
+
+class SavGol(object):
+
+    def __init__(self, 
+                window_size:int = 9, # recommended 7, 9, 11
+                polyprder:int = 2 # recommended 2, 3
+                ):
+        self.window_size = window_size
+        self.polyorder
+
+        
+    def __call__(self, positions:dict):
+        smoothed_positions = {}
+        for lm in positions:
+            N = len(positions[lm])
+
+            window_size = min(self.window_size, N)
+
+
+            x = [positions[lm][i][0] for i in range(N)]
+            y = [positions[lm][i][1] for i in range(N)]
+            input_array = np.stack([x, y], axis=1)
+            # applyging filter
+            coords_smooth = savgol_filter(
+                input_array,
+                window_length=window_size,
+                polyorder=self.polyorder,
+                axis=0,  # smooth over time
+                mode="interp"
+            )
+            x_smoothed = [coords_smooth[i][0] for i in range(len(coords_smooth))]
+            y_smoothed = [coords_smooth[i][1] for i in range(len(coords_smooth))]
+            coords_smoothed = [[float(x), float(y)] for x,y in zip(x_smoothed,y_smoothed)]
+            smoothed_positions[lm] = coords_smooth
+        return smoothed_positions
+
+
+
+

79a257251a27b64ade2b45784ff741599aa81d80
NicHrg <nico.herrig@gmx.de>
2026-06-14 15:57:45 +0200

    adjusted saving and load of KineticBody class

--- scripts/run_pose_estimation.py
+++ scripts/run_pose_estimation.py
@@ -8,13 +8,46 @@ sys.path.insert(0, ROOT_DIR)
 file_path = "./data/angle_test.mov"
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Run pose estimation on a video."
+    )
+
+    parser.add_argument(
+        "input_path",
+        type=Path,
+        help="Path to the input video file",
+        required=True
+    )
+
+    parser.add_argument(
+        "output_path",
+        type=Path,
+        help="Path to save positions from KineticBody object.",
+        required=True
+    )
+
+    return parser.parse_args()
+
+
 
 if __name__ == "__main__":
+
+    args = parse_args()
+
+    if not args.input_path.exists():
+        raise FileNotFoundError(
+            f"Input file does not exist: {args.input_path}"
+        )
+
+    args.output_path.parent.mkdir(parents=True, exist_ok=True)
+    
+
     body = run_pose_estimation(
-        input_path=file_path,
+        input_path=args.input_path,
         lag_reduction=True,
         apply_filter=True
     )
 
-    print(body)
+    
 
